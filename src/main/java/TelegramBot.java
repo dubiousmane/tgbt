b58c31da75b01648b420678a2f84d08a2e2c3fef
dubiousmane <dubiousmane@gmail.com>
2019-11-03 13:53:15 +0600

    bot can echo to your messages

---
+++ src/main/java/TelegramBot.java
@@ -0,0 +1,34 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return "dubio_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "931077832:AAFp-NT4n_N5NB3V8i9tHOlW8HK6RyHEl6M";
+    }
+}
